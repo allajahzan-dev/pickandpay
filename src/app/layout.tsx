9296a557a248a59cd2ce7ef76bf092f02d3a71e7
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-10-25 15:37:08 +0530

    patch: gave fixed theme as light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 export const metadata: Metadata = {
     title: "Pick-and-Pay",
     description: "Supermarket brand",
+    themeColor: "#ffffff",
 };
 
 export default function RootLayout({
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <meta name="color-scheme" content="light" />
+                <meta name="supported-color-schemes" content="light" />
+            </head>
             <body className={cn(urbanist.className, "antialiased")}>
                 <SmoothWrapper>{children}</SmoothWrapper>
             </body>
--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -12,3 +12,8 @@ export const sectionXpadding = "px-5 sm:px-10 xl:px-32";
 export const sectionClassName = `h-auto bg-white relative py-16 flex flex-col gap-8 justify-center 
 overflow-hidden will-change-transform 
 md:py-24 xl:py-32 ${sectionXpadding}`;
+
+// Text colors
+export const textMainColor = "text-[#262626]"
+export const textSubColor = "text-[#575757]"
+export const textSpcColor = "text-[#58655a]"
